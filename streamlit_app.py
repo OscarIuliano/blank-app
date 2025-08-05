6bc5042272d507e0c372a9be6ae243c01157d881
OscarIuliano <oiuliano90@gmail.com>
2025-08-05 12:33:09 +0000

    git cambio title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,26 +3,28 @@ import streamlit as st
 
 tab1, tab2, tab3 = st.tabs(["Gatto", "Cane", "Gufo"])
 
-st.title("Calcolatrice di prova 2")
+with tab1:
+    st.header("Questo è un gatto")
+    st.title("Calcolatrice di prova 2")
 
-num1 = st.number_input("Primo numero")
-operatore = st.selectbox("Operazione", ["+", "-", "*", "/"])
-num2 = st.number_input("Secondo numero")
-vocale = st.audio_input("registra vocale")
+    num1 = st.number_input("Primo numero")
+    operatore = st.selectbox("Operazione", ["+", "-", "*", "/"])
+    num2 = st.number_input("Secondo numero")
+    vocale = st.audio_input("registra vocale")
 
-if st.button("Calcola"):
-    try:
-        if operatore == "+":
-            risultato = num1 + num2
-        elif operatore == "-":
-            risultato = num1 - num2
-        elif operatore == "*":
-            risultato = num1 * num2
-        elif operatore == "/":
-            risultato = num1 / num2
-        st.success(f"Risultato operazione: {risultato}")
-    except ZeroDivisionError:
-        st.error("Errore: divisione per zero")
+    if st.button("Calcola"):
+        try:
+            if operatore == "+":
+                risultato = num1 + num2
+            elif operatore == "-":
+                risultato = num1 - num2
+            elif operatore == "*":
+                risultato = num1 * num2
+            elif operatore == "/":
+                risultato = num1 / num2
+            st.success(f"Risultato operazione: {risultato}")
+        except ZeroDivisionError:
+            st.error("Errore: divisione per zero")
 
-if st.button('Riproduci') :
-    output = st.audio(vocale, format="audio/mpeg", loop=False)
+    if st.button('Riproduci') :
+        output = st.audio(vocale, format="audio/mpeg", loop=False)
